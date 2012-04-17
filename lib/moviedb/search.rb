104962b9b3f604737446cc10dbd61f78b55f8c8e
Kristian Rasmussen <kr@krconsulting.dk>
2012-04-17 00:50:34 -0700

    removed filter stuff

--- lib/moviedb/search.rb
+++ lib/moviedb/search.rb
@@ -3,22 +3,5 @@ module MovieDB
   class Search
 
 
-    def filter_file(file)
-      
-      idx = file.rindex('.')
-      filebase = nil
-      extension = nil
-      if idx.nil? 
-        puts file
-        file.gsub(/[^A-Za-z0-9\-_]/, "")
-      else
-        filebase = file[0..idx] 
-        extension = file[idx..file.length] if !idx.nil?
-        filebase.gsub!(/[^A-Za-z0-9\-_]/, "")
-        extension.gsub!(/[^A-Za-z0-9\-_]/, "")
-        "#{filebase}.#{extension}"
-      end
-    end
-
   end
 end
